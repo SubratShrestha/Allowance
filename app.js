509558a6ab03240fc3b4fe8cc721b899d01b8591
subrat shrestha <z5266273@ad.unsw.edu.au>
2020-05-12 14:26:12 +1000

    Implementing Module pattern

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('hello there.');
+var budgetController = (function () {
+    // Code for budgetController.
+
+    // Returning an object.
+    return {
+        // All variables and methods that we want accessible later.
+    }
+})();
+
+var UIController = (function () {
+    // Code for UI controller.
+
+    // Returning an object.
+    return {
+        // All variables and methods that we want accessible later.
+    }
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+    // Code connecting the budget and UI controllers. Takes the objects returned by them as arguments.
+    // Renaming arguments of this function to something else so that its more independent.
+    // If budget and UI controllers were renamed, only need to change once at the bottom, everything else
+    // would still be called budgetCtrl and UICtrl.
+
+    // Returning an object.
+    return {
+        // All variables and methods that we want accessible later.
+    }
+})(budgetController, UIController);
