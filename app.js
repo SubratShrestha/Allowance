432074b77925ae6428815e703db82ec81624e044
subrat shrestha <z5266273@ad.unsw.edu.au>
2020-05-13 16:19:07 +1000

    created data structure and finished adding function into structure

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
+// Controller to keep track of income, expenses and the budget.
 var budgetController = (function () {
     // Code for budgetController.
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    // Data structure.
+    var data = {
+        items: {
+            inc: [],
+            exp: []
+        },
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+    }
+
 
     // Returning an object.
     return {
         // All variables and methods that we want accessible later.
+        addItem: function (type, desc, value) {
+            if (data.items[type].length > 0)
+                var ID = data.items[type][data.items[type].length - 1] + 1;
+            else var ID = 0;
+
+            if (type === 'inc')
+                newItem = new Income(ID, desc, value);
+            else newItem = new Expense(ID, desc, value);
+
+            data.items[type].push(newItem);
+        },
+        temp: function () {
+            return data;
+        }
     }
 })();
 
@@ -49,19 +88,21 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     // Get the input data.
     function ctrlAddItem() {
-        input = UICtrl.getInput();
-        console.log(input);
-    }
+        var input = UICtrl.getInput();
 
-    // add item to budget controller.
+        // add item to budget controller.
+        budgetCtrl.addItem(input.type, input.desc, input.val);
 
-    // add item to UI.
+        // add item to UI.
 
-    // calculate budget.
+        // calculate budget.
 
-    // display budget.
+        // display budget.
 
-    // Returning an object.
+        // Returning an object.
+    };
+
+
     return {
         // All variables and methods that we want accessible later.
         init: function () {
